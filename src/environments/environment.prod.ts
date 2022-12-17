c0c6ede0c585f63dfb07295298ecb3b56a4a54d5
Martha Tarantino <66340960+Martu-t@users.noreply.github.com>
2022-12-17 19:58:52 -0300

    update backend endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://portfolio-backend-production-afdf.up.railway.app/'
+  apiBaseUrl: 'https://portfolio-backend-production-afdf.up.railway.app'
 };
